97f43ef74a561e8ba355bae740a4d483b8306429
ashkan Mohammadpour <ashkanmohammadpour29@gmail.com>
2024-01-17 11:35:07 +0330

    removeTodo , addTodo , createTodo

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,53 @@
 "use strict";
-console.log('log in app');
+const formTodo = document.getElementById('form-todo');
+const textTodo = document.getElementById('title-form');
+const textErorr = document.getElementById('text-error');
+const tbody = document.getElementById('tbody');
+class Todo {
+    constructor(todo) {
+        this.id = todo.id,
+            this.title = todo.title,
+            this.status = todo.status;
+    }
+}
+class Ui {
+    addTodoList(todo) {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <th scope="row">${todo.id}</th>
+        <td>${todo.title}</td>
+        <td>
+            <input class="form-check-input checkbox" type="checkbox" value="">
+        </td>
+        <td><button type="button" onclick=(ui.removeTodo(event)) class="btn btn-danger">Danger</button></td>
+        `;
+        tbody.appendChild(tr);
+    }
+    removeTodo(event) {
+        var _a, _b;
+        // console.log(event.target);
+        const elelment = event.target;
+        (_b = (_a = elelment.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.remove();
+    }
+}
+let ui = new Ui();
+formTodo.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (textTodo.value.trim() === '') {
+        textErorr.innerHTML = 'لطفا متنی را وارد کنید';
+    }
+    else {
+        // console.log(textTodo.value);
+        textErorr.innerHTML = '';
+        const value = {
+            id: Math.round(Math.random() * 100),
+            title: textTodo.value.trim(),
+            status: false
+        };
+        // console.log(value);
+        const todoObj = new Todo(value);
+        // console.log(todoObj);
+        const todo = ui.addTodoList(todoObj);
+    }
+    textTodo.value = '';
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,74 @@
-console.log('log in app');
+const formTodo = document.getElementById('form-todo') as HTMLFormElement ;
+const textTodo = document.getElementById('title-form') as HTMLInputElement;
+const textErorr = document.getElementById('text-error') as HTMLDivElement;
+const tbody = document.getElementById('tbody')!
+
+interface TodoInterface{
+    id: number;
+    title: string;
+    status: boolean;
+}
+class Todo implements TodoInterface{
+    id: number
+    title: string
+    status: boolean
+
+    constructor(todo: TodoInterface){
+        this.id = todo.id,
+        this.title = todo.title,
+        this.status = todo.status
+    }
+}
+class Ui{
+    addTodoList(todo: TodoInterface){
+        const tr = document.createElement('tr')
+        tr.innerHTML = `
+        <th scope="row">${todo.id}</th>
+        <td>${todo.title}</td>
+        <td>
+            <input class="form-check-input checkbox" type="checkbox" value="">
+        </td>
+        <td><button type="button" onclick=(ui.removeTodo(event)) class="btn btn-danger">Danger</button></td>
+        `
+        tbody.appendChild(tr)
+    }
+    removeTodo(event: Event){
+        // console.log(event.target);
+        const elelment = event.target as HTMLElement;
+        elelment.parentElement?.parentElement?.remove();
+        
+    }
+}
+
+let ui = new Ui()
+
+formTodo!.addEventListener('submit' , (e: Event)=>{
+    e.preventDefault()
+    if(textTodo.value.trim() === ''){
+        
+        textErorr.innerHTML = 'لطفا متنی را وارد کنید'
+        
+    }else{
+        // console.log(textTodo.value);
+        textErorr.innerHTML = ''
+        const value: TodoInterface = {
+            id: Math.round(Math.random() * 100),
+            title: textTodo.value.trim(),
+            status: false
+        }
+        // console.log(value);
+        const todoObj = new Todo(value)
+        // console.log(todoObj);
+        const todo = ui.addTodoList(todoObj);
+        
+        
+       
+        
+        
+        
+        
+    }
+    textTodo.value = ''
+    
+    
+}) 
